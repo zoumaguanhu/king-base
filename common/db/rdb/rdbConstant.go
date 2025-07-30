0e556259c89e7545de22aec032c4b34c60023fa6
king <834855346.qq.com>
2025-07-30 23:19:27 +0800

    日常提交

--- common/db/rdb/rdbConstant.go
+++ common/db/rdb/rdbConstant.go
@@ -68,6 +68,7 @@ const (
 	PAY_METHOD_INFO   = "site:vsite:common:pay_method:pay_info"
 	LOCATION_API_INFO = "site:vsite:common:location_api:location_info"
 	SHOPPING_API_HASH = "site:vsite:common:shopping_api:shopping_hash"
+	SHOPPING_API_INFO = "site:vsite:common:shopping_api:shopping_info"
 )
 const (
 	STAT_PV      = "pv"
