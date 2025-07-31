8a651c68753b0e1fc2c6cc09f68787e04bdabe60
king <834855346.qq.com>
2025-07-31 17:52:46 +0800

    日常提交

--- common/constants/orderConstatns.go
+++ common/constants/orderConstatns.go
@@ -20,3 +20,19 @@ const (
 	ORDER_PART_REFUND = 2
 	ORDER_ALL_REFUND  = 3
 )
+const (
+	EVENT_TYPE_1 = 1 //创建订单
+	EVENT_TYPE_2 = 2 //用户支付完成
+	EVENT_TYPE_3 = 3 //商户发货
+	EVENT_TYPE_4 = 4 //已签收
+	EVENT_TYPE_5 = 5 //已取消
+	EVENT_TYPE_6 = 6 //部分退货
+	EVENT_TYPE_7 = 7 //全部退货
+	EVENT_TYPE_8 = 8 //退货完成
+)
+const (
+	OPERATOR_TYPE_SYS      = 0 //系统
+	OPERATOR_TYPE_MEMBER   = 1 //用户
+	OPERATOR_TYPE_CUSTOMER = 2 //客服
+	OPERATOR_TYPE_MERCHANT = 3 //商家
+)
